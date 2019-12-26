82fb252af23840978a98119416eca39d8cb311aa
Carlos Rodriguez <carlosandres.rodriguezpatino@gmail.com>
2019-12-26 20:46:48 +0100

    Initial commit

---
+++ src/main/java/com/carpco/petcity/PetcityMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.carpco.petcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcityMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcityMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carpco/petcity/PetcityMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpco.petcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcityMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
